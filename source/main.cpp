8e712dcdeb702b20fbf5b8f273b8502fc6508e39
tom <thombetteridge@gmail.com>
2026-03-24 19:15:06 +1030

    crash fix

--- source/main.cpp
+++ source/main.cpp
@@ -110,7 +110,8 @@ auto main() -> int
                                           ImVec2 { avail.x - right_margin, avail.y - bottom_margin },
                                           ImGuiInputTextFlags_AllowTabInput)) {
 
-               output_text = run_calc(input_buffer.data(), input_buffer.size());
+               output_text        = run_calc(input_buffer.data(), input_buffer.size());
+               output_text.back() = '\000';
             }
          }
       }
